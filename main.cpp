150d40f8d08a40b1eff63b8b8c51659a67beb296
devykrishna <48645293+devykrishna@users.noreply.github.com>
2022-08-16 15:28:39 +0400

    Update main.cpp
    Added logical error fixex

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ class Theatre {
     public:
         Theatre(){}
         Theatre(string strTheatre):TheatreName(strTheatre), seats{0},seatno(0),vacantseats(0) {}
+        
         string GetTName() {
             return TheatreName;
         } 
@@ -47,9 +48,10 @@ class Movies {
                return MovieName;
         } 
         ~Movies() {
-            for (int i = 0; i < theatre.size(); i++) {
-                       delete theatre[i];
-                }
+            //for (int i = 0; i < theatre.size(); i++) {
+                //cout << "destructor deleting theatre" << endl;
+                //delete theatre[i];
+           // }
         }                 
 }; 
 
@@ -69,9 +71,10 @@ class Services {
         int ShowSeatInfo(); 
         ~Services ()
         { 
-             for (int i = 0; i < movies.size(); i++) {
-                       delete movies[i];
-                }
+            for (int i = 0; i < movies.size(); i++) {
+               // cout << "destructor deleting movies" << endl;
+                delete movies[i];
+            }
         }             
 };
 
@@ -81,9 +84,9 @@ vector <string> Movies::GetTheatrelist(){
     vector <string> Tlist;
     vector <Theatre*> Tobj;
 
-   Tobj = GetTheatrePointer();
-   n = Tobj.size();
-  // cout << "theatrelist" << n;
+    Tobj = GetTheatrePointer();
+    n = Tobj.size();
+    // cout << "theatrelist" << n;
     for (int i = 0; i < n; i++) {
        // cout << "list theatre" << theatre[i]->GetTName() << endl;
         Tlist.push_back(theatre[i]->GetTName()); 
@@ -92,12 +95,10 @@ vector <string> Movies::GetTheatrelist(){
 }
 
 int Theatre:: CheckSeatAvailability () {
-int emptyseats = 0;
+    int emptyseats = 0;
     for(int i = 0;  i < 20; i++) {
         if (seats[i] == 0)
            emptyseats += 1;
-        //else 
-         //  emptyseats =  emptyseats - 1; 
     }
     vacantseats = emptyseats;
     return vacantseats;
@@ -111,7 +112,7 @@ bool Theatre:: BookTicket (int NumofTickets, vector <int> seatno) {
 }
 
 int Services::ListMovies() {
-    int n,ret = 0;
+    int n, ret = 0;
 
     n = movies.size();  
     //cout << "n =" << n << endl;
@@ -127,17 +128,19 @@ int Services::ListMovies() {
     cout << "Press any above number to selcet a Movie" << endl;
     cout << "To quit press 0" << endl;
     cin >> movieid;
-    if (movieid == 0)
+    if (movieid > n || movieid == 0) {
        ret = 0;
-    else if(isdigit(movieid))
+       cout << "Invalid movie selection" << endl;
+    } else if(isdigit(movieid))
        ret = 0;
     else
        ret = 1;
+
     return ret;       
 }
 
 int Services::ListTheatre() {
-    int ret = 0,n;
+    int ret = 0, n;
     vector <string> list;
 
     cout << "Please select Theatre" << endl << endl;
@@ -152,41 +155,39 @@ int Services::ListTheatre() {
     cout << "Press any above number to selcet a Theatre" << endl;
     cout << "To quit press 0" << endl;
     cin >> theatreid;
-    if (theatreid == 0)
+    if (theatreid == 0 || theatreid > n) {
+        cout << "Invalid movie selection : quits" << endl;
        ret = 0;
-    else
+    } else
        ret = 1;
-return ret;
+    return ret;
 }
 
 int Services:: ShowSeatInfo () {
-        int availtickets,seats,blocktickets;
-        char proceed,proceed1;
+        int availtickets, seats, blocktickets;
+        char proceed, proceed1;
         bool result;
         int *seatarray;
         vector <int> vectorseats;
         vector <int> tempseats;
-        //string tempMovie = " ";
-        //string tempTheatre  = " ";
         vector <Theatre*> Tobject;
-        //  n = sizeof(Tobj);
 
         Tobject = movies[movieid-1]->GetTheatrePointer();
         availtickets = Tobject[theatreid-1]->CheckSeatAvailability();
         cout << endl;
 
-again:  cout << availtickets << " no of tickets are available" << endl;
-        cout << "Please enter Number of tickets to book " << endl;
-        cin >> blocktickets;
-        if (blocktickets > availtickets) {
-            cout << blocktickets <<"Tickets are not available" <<  endl;
-            cout << "Do you want to continue y/n ?" << endl;
-            cin >> proceed;
-            if ( (proceed=='y') || (proceed=='Y') )
-               goto again;
-            else
-               goto ret;
-        }
+        do {
+            proceed = ' ';
+            cout << availtickets << " no of tickets are available" << endl;
+            cout << "Please enter Number of tickets to book " << endl;
+            cin >> blocktickets;
+            if (blocktickets > availtickets) {
+                cout << blocktickets <<" Tickets are not available" <<  endl;
+                cout << "Do you want to continue y/n ?" << endl;
+                cin >> proceed;
+            }
+        } while ((proceed=='y') || (proceed=='Y'));
+        
         cout << "\n\t Avaiable seats" << endl << endl;
         seatarray = Tobject[theatreid-1]->Getseats();
         for(int i = 0;  i < 20; i++) {
@@ -195,39 +196,33 @@ again:  cout << availtickets << " no of tickets are available" << endl;
             } else 
                   cout << '\t' << 'X';
             if (i % 2)
-                  cout << '\n';
-                
+                  cout << '\n';        
         }
-again1:    cout << "\nPlease select your :( " << blocktickets << ") seats " << endl;
+
+        do {
+            proceed1 = ' ';
+            cout << "\nPlease select your (" << blocktickets << ") seats " << endl;
             tempseats.clear();
-        for(int j = 0 ; j < blocktickets; j++) {
-            cin >> seats;
-            tempseats.push_back(seats);
-        }
-        for (int j = 0 ; j < blocktickets; j++) { 
-           // cout << "selected seats " << tempseats[j];
-                if( seatarray[tempseats[j] - 1] == 0) {
-                    vectorseats.push_back(tempseats[j]- 1);
-                } else {
-                  cout << "The selected seat is not available . Please try again !" << endl;
-                  cout << "Do you want to continue y/n ?" << endl;
-                  cin >> proceed1;
-                  if ( (proceed1 == 'y') || (proceed1 == 'Y') )
-                     goto again1;
-                  else
-                     goto ret;
-                }
+            for(int j = 0 ; j < blocktickets; j++) {
+                cin >> seats;
+                tempseats.push_back(seats);
             }
-        //}
-        //cout << "seat verified" << endl;
-       // tempMovie = movies[movieid - 1]->GetMName();
-       // tempTheatre = Tobject[theatreid - 1]->GetTName();
+            for (int j = 0 ; j < blocktickets; j++) { 
+                    if( seatarray[tempseats[j] - 1] == 0) {
+                        vectorseats.push_back(tempseats[j]- 1);
+                    } else {
+                      cout << "The selected seat is not available . Please try again !" << endl;
+                      cout << "Do you want to continue y/n ?" << endl;
+                      cin >> proceed1;
+                    }
+                }
+        } while ((proceed1 == 'y') || (proceed1 == 'Y'));
+
         cout << "Booking " << blocktickets << " Tickets for " << movies[movieid -1]->GetMName() << " in " << Tobject[theatreid-1]->GetTName() << endl; 
         result = Tobject[theatreid - 1]->BookTicket(blocktickets,vectorseats);
         if(result == true)
            return 1;
-   ret  : return 0;
-
+return 0;
 }
 
 int main()
@@ -241,16 +236,14 @@ int main()
     string strTheatrename = " ";
     string strMoviename = " ";
     string strName = " ";
-    char c;
-    int i = 0, j = 0;
+
 
     if (trial == 0) {
-        fstream Tfile,Mfile;
+        fstream Tfile, Mfile;
         Tfile.open("database1.csv",ios::in);
         if(!Tfile) {
             cout << "Database error" << endl; // verified
             Tfile.close();
-            //goto bye;
             return 0;
         } else {     
                 while( getline(Tfile, strTheatrename,',')) {
@@ -265,54 +258,53 @@ int main()
             Mfile.close();
             return 0;   
         } else {       
-                 while( getline(Mfile, strName)) {
-                       int s = 0;
-                     stringstream stringfile(strName) ;
-                     //cout << "Movie " << strMoviename << endl; 
-                      while( getline(stringfile, strName,',')) { 
+                while( getline(Mfile, strName)) {
+                        int s = 0;
+                        vTheatreNameList.clear();
+                        vObjTheatre.clear();
+                        strMoviename = " ";
+                        stringstream stringfile(strName) ;
+                        while( getline(stringfile, strName,',')) { 
                              if(s == 0) {
-                                 strMoviename = strName;
-                                 s = 1;
+                                strMoviename = strName;
+                                s = 1;
                              } else {
                                  //cout << "Theatre " << strName << endl;
                                  vTheatreNameList.push_back(strName);
                              }
-                      }
-                     for(int i = 0; i < vTheatreNameList.size(); i++) {
-                         for(int j = 0; j < vTheatreObjlist.size(); j++) {
-                             if(vTheatreObjlist[j]->GetTName() == vTheatreNameList[i]) {
+                        }
+                        for(int i = 0; i < vTheatreNameList.size(); i++) {
+                            for(int j = 0; j < vTheatreObjlist.size(); j++) {
+                                if(vTheatreObjlist[j]->GetTName() == vTheatreNameList[i]) {
                                  //cout << " list from main " << vTheatreNameList[i] << endl; 
                                  vObjTheatre.push_back(vTheatreObjlist[j]);
-                             }
-                         }
-                     }
-                 Movies *ptrMovieObj = new Movies(strMoviename,vObjTheatre);
-                 Book.pushMovies(ptrMovieObj);
-                   
+                                }
+                            }
+                        }
+                Movies *ptrMovieObj = new Movies(strMoviename, vObjTheatre);
+                //cout << "Movie " << strMoviename << endl; 
+                Book.pushMovies(ptrMovieObj);
               } 
         }                             
-            Mfile.close();
-            Tfile.close();
-            trial = 1;
-        }  
+        Mfile.close();
+        Tfile.close();
+        trial = 1;
+    }  
     while (1) {
         if (Book.ListMovies()) {
            if (Book.ListTheatre()) {
                if (Book.ShowSeatInfo()) {
                     cout<<"\n\n Tickets has been succcesfully booked for you !!!" ;
                     cout<<"\n\n THANK YOU !!!\n\n"; 
-               } else {
-                cout<<"\n\n Booking failed Please try again !!!";
-               }
+               } else 
+                cout << "\n\n Booking failed Please try again !!!";
            } else
                 break;
-        } else {
-          break;  
-        }
-
+        } else 
+            break;     
         sleep(5);
     }
-  //  bye:    
-    cout<<"\nTHANK YOU !!!";   
+    
+    cout << "\nTHANK YOU !!!" << endl;   
     return 0;
 }
